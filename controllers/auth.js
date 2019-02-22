fdd5c0228313c5bd2468c8c642520bd2251e5d99
panop <panop.mail.95@gmail.com>
2019-02-22 02:00:44 +0200

    Add loginUser() on auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,19 +8,45 @@ const Helpers = require('../helpers/helpers');
 const dbConfig = require('../config/secret');
 
 module.exports = {
-  loginUser(req, res) {
+  async loginUser(req, res) {
     console.log(req.body);
-    const schema = Joi.object().keys({
-      username: Joi.string().required(),
-      password: Joi.string().required()
-    });
 
-    const { error, value } = Joi.validate(req.body, schema);
-    if (error && error.details) {
+    if (!req.body.username || !req.body.password) {
       return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ msg: error.details });
+        .status(HttpStatus.NOT_FOUND)
+        .json({ message: 'No empty fields allowed' });
     }
+
+    await User.findOne({
+      username: Helpers.lowerCase(req.body.username)
+    })
+      .then(user => {
+        if (!user) {
+          return res
+            .status(HttpStatus.NOT_FOUND)
+            .json({ message: 'Username not found' });
+        }
+
+        return bcrypt.compare(req.body.password, user.password).then(result => {
+          if (!result) {
+            return res
+              .status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .json({ message: 'Password is incorrect' });
+          }
+          const token = jwt.sign({ data: user }, dbConfig.secret, {
+            expiresIn: 10000
+          });
+          res.cookie('auth', token);
+          return res
+            .status(HttpStatus.OK)
+            .json({ message: 'Login successful', user, token });
+        });
+      })
+      .catch(err => {
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Error occured' });
+      });
   },
 
   async createUser(req, res) {
@@ -35,9 +61,7 @@ module.exports = {
 
     const { error, value } = Joi.validate(req.body, schema);
     if (error && error.details) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ msg: error.details });
+      return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details });
     }
 
     const uname = await User.findOne({
